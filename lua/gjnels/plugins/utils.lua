d6c2dbdc598444acb8a519ce52c8554f79b61f90
Garrett Nelson <gjnels@proton.me>
2023-05-25 13:50:21 -0400

    Add utility plugins to be used by other plugins

---
+++ lua/gjnels/plugins/utils.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    'nvim-lua/plenary.nvim',
+    lazy = true,
+  },
+
+  {
+    'MunifTanjim/nui.nvim',
+    lazy = true,
+  },
+
+  {
+    'dstein64/vim-startuptime',
+    cmd = 'StartupTime',
+  },
+
+  {
+    'nvim-tree/nvim-web-devicons',
+    lazy = true,
+  },
+}
