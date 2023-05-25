1df61164f69e7b699d7fb163a04f07915eff52f7
Garrett Nelson <gjnels@proton.me>
2023-05-25 14:56:12 -0400

    Add auto pairs and comments plugins

---
+++ lua/gjnels/plugins/coding.lua
@@ -0,0 +1,27 @@
+return {
+  -- auto pairs: {} [] () '' "" ``
+  {
+    'echasnovski/mini.pairs',
+    event = 'VeryLazy',
+    config = function(_, opts)
+      require('mini.pairs').setup(opts)
+    end,
+  },
+
+  -- comments
+  { 'JoosepAlviste/nvim-ts-context-commentstring', lazy = true },
+  {
+    'echasnovski/mini.comment',
+    event = 'VeryLazy',
+    opts = {
+      hooks = {
+        pre = function()
+          require('ts_context_commentstring.internal').update_commentstring({})
+        end,
+      },
+    },
+    config = function(_, opts)
+      require('mini.comment').setup(opts)
+    end,
+  },
+}
