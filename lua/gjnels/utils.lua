939fc25ffe675867e5d5f73db02e34909ccedfc0
Garrett Nelson <gjnels@proton.me>
2023-05-26 12:06:24 -0400

    Add float_term function

--- lua/gjnels/utils.lua
+++ lua/gjnels/utils.lua
@@ -188,4 +188,17 @@ function M.toggle_diagnostics()
   end
 end
 
+-- Opens a floating terminal (interactive by default)
+---@param cmd? string[]|string
+---@param opts? LazyCmdOptions|{interactive?:boolean, esc_esc?:false}
+function M.float_term(cmd, opts)
+  opts = vim.tbl_deep_extend('force', {
+    size = { width = 0.9, height = 0.9 },
+  }, opts or {})
+  local float = require('lazy.util').float_term(cmd, opts)
+  if opts.esc_esc == false then
+    vim.keymap.set('t', '<esc>', '<esc>', { buffer = float.buf, nowait = true })
+  end
+end
+
 return M
